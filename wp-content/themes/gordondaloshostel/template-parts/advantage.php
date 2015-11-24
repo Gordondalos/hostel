c6752395f44c4e988b4821498699fa1ff40b2ccd
gordondalos <gordondalos@gmail.com>
2015-11-25 02:12:35 +0600

    Вставил блок advantage

--- wp-content/themes/gordondaloshostel/template-parts/advantage.php
+++ wp-content/themes/gordondaloshostel/template-parts/advantage.php
@@ -1,106 +1,134 @@
-<section class="well well1 parallax text-center" data-url="<?php bloginfo('template_directory')?>/img/parallax2.jpg" data-mobile="true"
-         data-speed="0.6">
-    <div class="container">
-        <h2>
-            Our offerings
-        </h2>
+<?php
+$query1 = new WP_Query('page_id=44');
+while($query1->have_posts()) $query1->the_post(); ;?>
+<div class="entry-content">
+    <?php the_content(); ?>
+</div> <?php wp_reset_query();
+?>
 
-        <div class="row">
 
-            <ul class="icon-list">
 
-                <li class="col-md-4 col-sm-6 col-sx-12 wow fadeInLeft" data-wow-delay=".3s" >
-                    <span class="fa-bell"></span>
 
-                    <h6>
-                        <a href="#">
-                            Nulla venenatis
-                        </a>
-                    </h6>
-                    <p class="offs2">
-                        Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis
-                        dis parturient <br/>
-                        montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio.
-                    </p>
-                </li>
-
-                <li class="col-md-4 col-sm-6 col-sx-12 wow fadeInLeft" data-wow-delay=".2s" >
-                    <span class="fa-book"></span>
-
-                    <h6>
-                        <a href="#">
-                            Lorem ipsum dolor
-                        </a>
-                    </h6>
-                    <p class="offs2">
-                        Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis
-                        dis parturient <br/>
-                        montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio.
-                    </p>
-                </li>
-
-                <li class="col-md-4 col-sm-6 col-sx-12 wow fadeInLeft" data-wow-delay=".1s" >
-                    <span class="fa-bus"></span>
-
-                    <h6>
-                        <a href="#">
-                            Consectetuer
-                        </a>
-                    </h6>
-                    <p class="offs2">
-                        Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis
-                        dis parturient <br/>
-                        montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio.
-                    </p>
-                </li>
-
-                <li class="col-md-4 col-sm-6 col-sx-12 wow fadeInRight" data-wow-delay=".1s">
-                    <span class="fa-calendar"></span>
-
-                    <h6>
-                        <a href="#">
-                            Aliquet sit amet
-                        </a>
-                    </h6>
-                    <p class="offs2">
-                        Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis
-                        dis parturient <br/>
-                        montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio.
-                    </p>
-                </li>
-
-                <li class="col-md-4 col-sm-6 col-sx-12 wow fadeInRight" data-wow-delay=".2s">
-                    <span class="fa-coffee"></span>
-
-                    <h6>
-                        <a href="#">
-                            Sed in lacus
-                        </a>
-                    </h6>
-                    <p class="offs2">
-                        Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis
-                        dis parturient <br/>
-                        montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio.
-                    </p>
-                </li>
-
-                <li class="col-md-4 col-sm-6 col-sx-12 wow fadeInRight" data-wow-delay=".3s">
-                    <span class="fa-hotel"></span>
-
-                    <h6>
-                        <a href="#">
-                            Aliquet sit amet
-                        </a>
-                    </h6>
-                    <p class="offs2">
-                        Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis
-                        dis parturient <br/>
-                        montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio.
-                    </p>
-                </li>
-
-            </ul>
-
-        </div>
-    </div>
-</section>
+<!---->
+<!--<section class="well well1 parallax text-center" data-url="--><?php //bloginfo('template_directory')?><!--/img/parallax2.jpg" data-mobile="true"-->
+<!--         data-speed="0.6">-->
+<!--    <div class="container">-->
+<!--        <h2>-->
+<!--            Our offerings-->
+<!--        </h2>-->
+<!---->
+<!--        <div class="row">-->
+<!---->
+<!--            <ul class="icon-list">-->
+<!---->
+<!---->
+<!---->
+<!---->
+<!---->
+<!---->
+<!---->
+<!---->
+<!--                <li class="col-md-4 col-sm-6 col-sx-12 wow fadeInLeft" data-wow-delay=".3s" >-->
+<!--                    <span class="fa-bell"></span>-->
+<!---->
+<!--                    <h6>-->
+<!--                        <a href="#">-->
+<!--                            Nulla venenatis-->
+<!--                        </a>-->
+<!--                    </h6>-->
+<!--                    <p class="offs2">-->
+<!--                        Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis-->
+<!--                        dis parturient <br/>-->
+<!--                        montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio.-->
+<!--                    </p>-->
+<!--                </li>-->
+<!---->
+<!--                <li class="col-md-4 col-sm-6 col-sx-12 wow fadeInLeft" data-wow-delay=".2s" >-->
+<!--                    <span class="fa-book"></span>-->
+<!---->
+<!--                    <h6>-->
+<!--                        <a href="#">-->
+<!--                            Lorem ipsum dolor-->
+<!--                        </a>-->
+<!--                    </h6>-->
+<!--                    <p class="offs2">-->
+<!--                        Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis-->
+<!--                        dis parturient <br/>-->
+<!--                        montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio.-->
+<!--                    </p>-->
+<!--                </li>-->
+<!---->
+<!--                <li class="col-md-4 col-sm-6 col-sx-12 wow fadeInLeft" data-wow-delay=".1s" >-->
+<!--                    <span class="fa-bus"></span>-->
+<!---->
+<!--                    <h6>-->
+<!--                        <a href="#">-->
+<!--                            Consectetuer-->
+<!--                        </a>-->
+<!--                    </h6>-->
+<!--                    <p class="offs2">-->
+<!--                        Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis-->
+<!--                        dis parturient <br/>-->
+<!--                        montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio.-->
+<!--                    </p>-->
+<!--                </li>-->
+<!---->
+<!---->
+<!---->
+<!---->
+<!---->
+<!---->
+<!---->
+<!---->
+<!---->
+<!--                -->
+<!--                <li class="col-md-4 col-sm-6 col-sx-12 wow fadeInRight" data-wow-delay=".1s">-->
+<!--                    <span class="fa-calendar"></span>-->
+<!---->
+<!--                    <h6>-->
+<!--                        <a href="#">-->
+<!--                            Aliquet sit amet-->
+<!--                        </a>-->
+<!--                    </h6>-->
+<!--                    <p class="offs2">-->
+<!--                        Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis-->
+<!--                        dis parturient <br/>-->
+<!--                        montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio.-->
+<!--                    </p>-->
+<!--                </li>-->
+<!---->
+<!--                <li class="col-md-4 col-sm-6 col-sx-12 wow fadeInRight" data-wow-delay=".2s">-->
+<!--                    <span class="fa-coffee"></span>-->
+<!---->
+<!--                    <h6>-->
+<!--                        <a href="#">-->
+<!--                            Sed in lacus-->
+<!--                        </a>-->
+<!--                    </h6>-->
+<!--                    <p class="offs2">-->
+<!--                        Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis-->
+<!--                        dis parturient <br/>-->
+<!--                        montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio.-->
+<!--                    </p>-->
+<!--                </li>-->
+<!---->
+<!--                <li class="col-md-4 col-sm-6 col-sx-12 wow fadeInRight" data-wow-delay=".3s">-->
+<!--                    <span class="fa-hotel"></span>-->
+<!---->
+<!--                    <h6>-->
+<!--                        <a href="#">-->
+<!--                            Aliquet sit amet-->
+<!--                        </a>-->
+<!--                    </h6>-->
+<!--                    <p class="offs2">-->
+<!--                        Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis-->
+<!--                        dis parturient <br/>-->
+<!--                        montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio.-->
+<!--                    </p>-->
+<!--                </li>-->
+<!---->
+<!--            </ul>-->
+<!---->
+<!--        </div>-->
+<!--    </div>-->
+<!--</section>-->
